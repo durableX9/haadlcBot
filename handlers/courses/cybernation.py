04cf664a4349dc9a6ab40175f9c0e5829454a2ca
Rasulov Shodiyor <108322250+ruymhdi@users.noreply.github.com>
2024-02-11 18:07:06 +0500

    updated

--- handlers/courses/cybernation.py
+++ handlers/courses/cybernation.py
@@ -1,7 +1,9 @@
 from aiogram import types
 
-from loader import dp
+from loader import dp, bot
+from keyboards.inline.cybernation_kb import cybernation_kb
+from aiogram.types import callback_query, CallbackQuery
 
 @dp.message_handler(commands='cybernation')
 async def cybernation_com(message: types.Message):
-    await message.answer("Cybernation kursi haqida to'liq ma'lumot uchun quyidagi tugmadan foydalaning!")
+    await message.reply("Cybernation kursi haqida to'liq ma'lumot uchun quyidagi tugmadan foydalaning!", reply_markup=cybernation_kb)
