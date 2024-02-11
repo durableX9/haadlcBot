d55c97088a884133630abbad5a357953d5406873
Rasulov Shodiyor <108322250+ruymhdi@users.noreply.github.com>
2024-02-11 16:46:22 +0500

    first commit

---
+++ utils/set_bot_commands.py
@@ -0,0 +1,16 @@
+from aiogram import types
+
+async def set_default_commands(dp):
+    await dp.bot.set_my_commands(
+        [
+            types.BotCommand("start", "Botni Ishga Tushirish"),
+            types.BotCommand("help", "Ko'rsatma"),
+            types.BotCommand("aboutus", "Biz Haqimizda"),
+            types.BotCommand("aboutadresses", "Bizning Manzillar"),
+            types.BotCommand("cybernation", "Cybernation Kursi Haqida Ma'lumot"),
+            types.BotCommand("flutter", "Flutter Kursi Haqida Ma'lumot"),
+            types.BotCommand("robohack", "Robohack Kursi Haqida Ma'lumot"),
+            types.BotCommand("netbackend", ".NET BackEnd Kursi Haqida Ma'lumot"),
+            types.BotCommand("cybersecurity", "CyberSecurity Kursi Haqida Ma'lumot")
+        ]
+    )
