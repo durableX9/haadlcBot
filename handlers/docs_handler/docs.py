650eba8b504ea781cb8bd5175f3a79317e5fe39f
Rasulov Shodiyor <108322250+ruymhdi@users.noreply.github.com>
2024-02-12 14:16:16 +0500

    updated docs

---
+++ handlers/docs_handler/docs.py
@@ -0,0 +1,14 @@
+from loader import dp, bot
+from aiogram.types import ContentType, Message
+
+
+@dp.message_handler()
+async def text_handler(message: Message):
+    await message.reply('Siz Matn Yubordingiz!')
+
+@dp.message_handler(content_types=ContentType.VIDEO)
+async def doc_handler(message: Message):
+    await message.video.download()
+    await message.reply("Video Qabul Qilindi Ammo Siz Video yubordingiz!\n"
+                        f"file_id = {message.video.file_id}")
+    
