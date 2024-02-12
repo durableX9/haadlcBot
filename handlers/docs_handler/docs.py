8992009d0e7364f58363c1b906d4d75c4664a594
Rasulov Shodiyor <108322250+ruymhdi@users.noreply.github.com>
2024-02-12 14:38:56 +0500

    updated docs

--- handlers/docs_handler/docs.py
+++ handlers/docs_handler/docs.py
@@ -1,4 +1,4 @@
-from loader import dp, bot
+from loader import dp
 from aiogram.types import ContentType, Message
 
 
@@ -9,6 +9,6 @@ async def text_handler(message: Message):
 @dp.message_handler(content_types=ContentType.VIDEO)
 async def doc_handler(message: Message):
     await message.video.download()
-    await message.reply("Video Qabul Qilindi Ammo Siz Video yubordingiz!\n"
+    await message.reply("<b>Video Qabul Qilindi Ammo Siz Video yubordingiz!\n</b>"
                         f"file_id = {message.video.file_id}")
     
