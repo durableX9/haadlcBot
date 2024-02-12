650f7659a6908c75eb79693e0b3e5241e12b6e90
Rasulov Shodiyor <108322250+ruymhdi@users.noreply.github.com>
2024-02-12 14:16:23 +0500

    updated cybernation_kb

--- keyboards/inline/cybernation_kb.py
+++ keyboards/inline/cybernation_kb.py
@@ -1,8 +1,3 @@
-# from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
-
-# cybernation_kb = InlineKeyboardButton("Batafsil ma'lumot", url='https://telegra.ph/CyberNation-0tio4', callback_data='cybernation_kb')
-# start_keyboard = InlineKeyboardMarkup(resize_keyboard=True).add(cybernation_kb)
-# cybernation_kb.py
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 cybernation_kb = InlineKeyboardMarkup(resize_keyboard=True)
