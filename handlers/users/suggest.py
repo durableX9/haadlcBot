775ad9f1bf4459c64541c8fd33ac34d0d7a9f1a7
Rasulov Shodiyor <108322250+ruymhdi@users.noreply.github.com>
2024-02-14 15:03:00 +0500

    updated suggest

--- handlers/users/suggest.py
+++ handlers/users/suggest.py
@@ -19,4 +19,25 @@ async def procces_write(message: types.Message, state: FSMContext):
      taklif = f"<b>Foydalanuvchidan kelgan taklif yoki shikoyat:</b> {write}"
 
      await bot.send_message(chat_id='-1002103258715', text=f"{taklif}")
+     text = ("@haad_uz Programming and Cyber & Security Learning Center!\n",
+                "<b>Asosiy:</b>",
+                "/start - Botni Ishga Tushirish\n"
+                "/help - Shu Habarni Ko'rsatadi\n\n"
+                "<b>Biz Haqimizda:</b>\n"
+                "/aboutus - Biz Haqimizda\n"
+                "/aboutadresses - Bizning manzillar\n\n"
+                "<b>Bizning Kurslar Haqida Ma'lumot:</b>\n"
+                "/cybernation - Cybernation Kursi\n"
+                "/cybersecurity - CyberSecurity Kursi\n"
+                "/netbackend - .NET BackEnd Kursi\n"
+                "/robohack - Robohack Kursi\n"
+                "/flutter - Flutter Kursi\n\n"
+                "<b>Kurslarga Ro'yxatdan O'tish:</b>\n"
+                "/register - Kurslarga Yozilish\n",
+                "<b>Sifat nazorati:</b>\n"
+                "/suggest - Taklif Va Shikoyat Uchun")
+
+     await message.answer("\n".join(text))
+
      await state.finish()
+
