7c77fe9c2750dd2a805ab87d6014aeac7574b5dd
Rasulov Shodiyor <108322250+ruymhdi@users.noreply.github.com>
2024-02-14 16:16:58 +0500

    updated start

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -12,3 +12,24 @@ async def start_command(message: types.Message):
             db.add_fullname(user_id=user_id, full_name=full_name)
 
     await message.answer(f"AssalomuAlaykum {message.from_user.full_name}")
+
+
+    text = ("@haad_uz Programming and Cyber & Security Learning Center!\n",
+            "<b>Asosiy:</b>",
+            "/start - Botni Ishga Tushirish\n"
+            "/help - Shu Habarni Ko'rsatadi\n\n"
+            "<b>Biz Haqimizda:</b>\n"
+            "/aboutus - Biz Haqimizda\n"
+            "/aboutadresses - Bizning manzillar\n\n"
+            "<b>Bizning Kurslar Haqida Ma'lumot:</b>\n"
+            "/cybernation - Cybernation Kursi\n"
+            "/cybersecurity - CyberSecurity Kursi\n"
+            "/netbackend - .NET BackEnd Kursi\n"
+            "/robohack - Robohack Kursi\n"
+            "/flutter - Flutter Kursi\n\n"
+            "<b>Kurslarga Ro'yxatdan O'tish:</b>\n"
+            "/register - Kurslarga Yozilish\n",
+            "<b>Sifat nazorati:</b>\n"
+            "/suggest - Taklif Va Shikoyat Uchun")
+
+    await message.answer("\n".join(text))
